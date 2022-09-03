a05d387077d950d9afb5cb3a27d80f914668f241
adrianm240 <106203262+adrianm240@users.noreply.github.com>
2022-09-03 15:50:42 -0400

    adding final branch file version.

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -6,7 +6,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -29,24 +29,22 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/adrianm240/Mapping_Earthquakes/main/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/adrianm240/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  fillColor: "yellow",
+  color: "blue",
+  weight: 1
 };
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-    style: myStyle,
-    onEachFeature: function(feature, layer){
-      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-      + feature.properties.dst + "</h3>");
-    }
-  }).addTo(map);
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+    L.geoJSON(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer){
+        layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+    }}).addTo(map);
 });
 
