11be41fec6b918cb8569d986c28de49d962c144f
adrianm240 <106203262+adrianm240@users.noreply.github.com>
2022-09-03 12:47:25 -0400

    updating branch code.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,25 +1,36 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Coordinates for each point to be used in the line.
-// Coordinates for each point to be used in the polyline.
-let line = [
-    [37.6213, -122.3790],
-    [30.197517388192765, -97.66624562367367],
-    [43.6777641359699, -79.6248197012369],
-    [40.64295535693164, -73.77537515146888]
-  ];
+// Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup("<h2>" + feature.properties.name + "</h2>");
+    }
 
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-    color: "blue",
-    weight: 4,
-    opacity: 0.5,
-    dashArray: '10,10'
- }).addTo(map);
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
